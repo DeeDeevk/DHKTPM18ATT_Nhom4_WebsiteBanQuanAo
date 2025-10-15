e93202e43000d03c1e89f6501a11a4d508ae8816
DeeDeevk <khoa01022020@gmail.com>
2025-10-15 22:11:00 +0700

    Init project

---
+++ kh3tshop-be/src/main/java/fit/iuh/kh3tshopbe/Kh3tshopBeApplication.java
@@ -0,0 +1,13 @@
+package fit.iuh.kh3tshopbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kh3tshopBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kh3tshopBeApplication.class, args);
+    }
+
+}

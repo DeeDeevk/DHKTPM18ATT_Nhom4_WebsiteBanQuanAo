a1b137e12bd1194a2070e7140b55e94d7bcce4a2
Khánh Gia <141854652+khanhlow1402@users.noreply.github.com>
2025-11-15 23:26:20 +0700

    Customer xem ds san pham (#14)
    * home_page
    
    * pull bai tu master ve local 11/7/2025 thekhanh
    
    * Pull tu master về local 11/12/2025
    
    * Push len nhanh fe-DuongKhanh 11/12/2025 9:45PM
    
    * vip
    
    * legiakhanh trang product ver1
    
    ---------
    
    Co-authored-by: TheKhanh1104 <22660101.khanh@student.iuh.edu.vn>
    Co-authored-by: Dương Thế Khánh <141980875+TheKhanh1104@users.noreply.github.com>

--- kh3tshop-be/src/main/java/fit/iuh/kh3tshopbe/service/CategoryService.java
+++ kh3tshop-be/src/main/java/fit/iuh/kh3tshopbe/service/CategoryService.java
@@ -1,14 +1,34 @@
 package fit.iuh.kh3tshopbe.service;
 
+import fit.iuh.kh3tshopbe.dto.response.CategoryResponse;
+import fit.iuh.kh3tshopbe.entities.Category;
 import fit.iuh.kh3tshopbe.repository.CategoryRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
-@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class CategoryService {
+
     CategoryRepository categoryRepository;
+
+    public List<CategoryResponse> getAllCategories() {
+        return categoryRepository.findAll()
+                .stream()
+                .map(this::convertToCategoryResponse)
+                .collect(Collectors.toList());
+    }
+
+    private CategoryResponse convertToCategoryResponse(Category category) {
+        return CategoryResponse.builder()
+                .id(category.getId())
+                .name(category.getName())
+                .build();
+    }
 }
--- kh3tshop-fe/src/App.jsx
+++ kh3tshop-fe/src/App.jsx
@@ -2,25 +2,24 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Layout from "./components/Layout"
-<<<<<<< HEAD
-import ForgotPassword from "./pages/ForgetPassword";
-=======
+import Layout from "./components/Layout";
 import About from "./pages/AboutUs";
->>>>>>> 029ff79ef83b4cc03934b8aa4a1618e2982ef677
+import ForgotPassword from "./pages/ForgetPassword";
+import Product from "./pages/Product";
 function App() {
   return (
     <>
       <Routes>
-       {/* Pages with Header and Footer */}
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About/>} />
-      </Route>
-      {/* Pages without Header and Footer */}
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/forget_password" element={<ForgotPassword />} />
+        {/* Pages with Header and Footer */}
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/product" element={<Product />} />
+        </Route>
+        {/* Pages without Header and Footer */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forget_password" element={<ForgotPassword />} />
       </Routes>
     </>
   );

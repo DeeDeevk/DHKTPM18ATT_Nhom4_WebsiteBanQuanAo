aaf0bbe4afac6ecd2df6d789d1e1c7d09e9a875d
Dương Thế Khánh <141980875+TheKhanh1104@users.noreply.github.com>
2025-11-16 22:05:56 +0700

    Customer xem chi tiet san pham đã xong  (#16)
    * pull bai tu master ve local 11/7/2025 thekhanh
    
    * Pull tu master về local 11/12/2025
    
    * Push len nhanh fe-DuongKhanh 11/12/2025 9:45PM
    
    * Hoan thanh trang Product-Detail 11/16 3:11PM
    
    ---------
    
    Co-authored-by: Phạm Văn Hinh <137275695+PhamHinh2004@users.noreply.github.com>

--- kh3tshop-fe/src/App.jsx
+++ kh3tshop-fe/src/App.jsx
@@ -2,24 +2,30 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Layout from "./components/Layout";
-import About from "./pages/AboutUs";
+import Layout from "./components/Layout"
 import ForgotPassword from "./pages/ForgetPassword";
+import About from "./pages/AboutUs";
+import Policy from "./pages/Policy";
 import Product from "./pages/Product";
+import ProductDetail from "./pages/ProductDetail";
+import Layout from "./components/Layout";
+import ForgotPassword from "./pages/ForgetPassword";
 function App() {
   return (
     <>
       <Routes>
-        {/* Pages with Header and Footer */}
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/product" element={<Product />} />
-        </Route>
-        {/* Pages without Header and Footer */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/forget_password" element={<ForgotPassword />} />
+       {/* Pages with Header and Footer */}
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/policy" element={<Policy/>} />
+        <Route path="/product" element={<Product/>}/>
+        <Route path="/product/:id" element={<ProductDetail/>} />
+      </Route>
+      {/* Pages without Header and Footer */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/forget_password" element={<ForgotPassword />} />
       </Routes>
     </>
   );
